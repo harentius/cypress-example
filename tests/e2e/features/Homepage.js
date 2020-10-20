e2f44c6724dcb42e4d2d13140dd5b723cb345d9b
Yaroslav Osypenko <yaroslav.osypenko@gmail.com>
2020-10-20 22:39:47 +0300

    codestyle

--- tests/e2e/features/Homepage.js
+++ tests/e2e/features/Homepage.js
@@ -3,6 +3,7 @@ import Homepage from '../support/business/Homepage';
 describe('Homepage', () => {
   it("Contains 'Sign in' Button", () => {
     Homepage.open();
+
     Homepage.assertSignInButton();
   })
 })
--- tests/e2e/features/Login.js
+++ tests/e2e/features/Login.js
@@ -3,8 +3,10 @@ import Login from '../support/business/Login';
 describe('Login', () => {
   it("Login with wrong credentials shows error message", () => {
     Login.open();
+
     Login.fillCredentials('wrong_name', 'wrong_password');
     Login.submitForm();
+
     Login.assertNotification('Incorrect username or password.');
   })
 })
